590af84b78b179329d54f5d29cdecd80b7318325
Hrithik2008 <64474232+Hrithik2008@users.noreply.github.com>
2021-12-19 11:15:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View,Image } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import FbScreen from './screens/fb';
+import InScreen from './screens/in';
+
+export default class App extends React.Component {
+  render(){
+    return (
+      
+        <AppContainer />
+      
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  FaceBook: {screen: FbScreen},
+  Instagram: {screen: InScreen},
+  })
+
+  const AppContainer =  createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
